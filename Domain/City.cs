0910ee8f036a5b7540e4f9d111cbd8b6706717f8
antonije-pavlovic <antonijepavlovic1@gmail.com>
2019-05-18 15:38:18 +0200

    git ignore

---
+++ Domain/City.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class City : BaseEntity
+    {
+        public String Name { get; set; }
+    }
+}
---
+++ Domain/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Restaurant : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Number { get; set; }
+        public City City { get; set; }
+        public int  CityId { get; set; }
+    }
+}
